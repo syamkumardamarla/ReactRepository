8865e5bedaf108cc0f70a25ef56588b8e19a2610
syamkumardamarla <syamcse5d7@gmail.com>
2024-11-18 23:29:11 -0800

    Updated react topics Practice

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,17 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-function Car() {
-  return <h2> car Component!!!</h2>;
+function Car(props) {
+  return (
+    <h2>
+      {" "}
+      Car Brand:{props.cardetails.name} year:{props.cardetails.year} fuel:
+      {props.cardetails.fuel}{" "}
+    </h2>
+  );
+}
+
+function Garage() {
+  ////usage of rest opeartor in Introduction Component
+  const Details = { name: "Ford", year: "2000", fuel: "petrol" }; //object
+  //child component
+  return (
+    <>
+      {" "}
+      <h2> Garage Componet </h2>;
+      <Car cardetails={Details} />
+      <Promotion name={Details.name} />
+      <Introduction {...Details} />
+      <CarEvent />
+      <CarBrands />
+    </>
+  );
+}
+
+function Introduction({ name, year, fuel }) {
+  return (
+    <div>
+      <p>Name:{name}</p>
+      <p>Age:{year}</p>
+      <p>fuel:{fuel}</p>
+    </div>
+  );
+}
+
+function CarEvent() {
+  //<> </> //React Fragment //react useState() hook usage using counter
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count - 5);
+        }}
+      >
+        -5
+      </button>
+      <span>{count}</span>
+      <button
+        onClick={() => {
+          if (count >= 0) {
+            setCount(count + 5);
+          }
+        }}
+      >
+        +5
+      </button>
+    </div>
+  );
+}
+
+function Promotion({ name }) {
+  return <h2> Introducing {name} </h2>;
+}
+
+function CarBrands() {
+  const [value, setValue] = useState("");
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter Car Brand"
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          setValue("");
+        }}
+      >
+        clear
+      </button>
+      <ul>{value}</ul>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Car />);
+root.render(<Garage />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
